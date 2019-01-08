856c070648e767b061991258ab60d519233d384a
Mon Maldonado <monterdi@gmail.com>
2019-01-08 23:51:01 +0100

    Lambdas

--- funciones1nivel.py
+++ funciones1nivel.py
@@ -1,8 +1,12 @@
 def normal(i):
     return i
 
+
 def cuadrado(x):
     return x * x
+
+def cubo(x):
+    return x**3
 
 
 def sumaTodos(limitTo, f):
@@ -12,5 +16,6 @@ def sumaTodos(limitTo, f):
         
     return resultado
 
-print(sumaTodos(100, normal))
-print(sumaTodos(3, cuadrado))
+if __name__ == '__main__':
+    print(sumaTodos(100, normal))
+    print(sumaTodos(3, cuadrado))
---
+++ funcionesLambda.py
@@ -0,0 +1,6 @@
+from funciones1nivel import sumaTodos
+
+print(sumaTodos(3, lambda x: x*2))
+print(sumaTodos(100, lambda x: x*3))
+
+
