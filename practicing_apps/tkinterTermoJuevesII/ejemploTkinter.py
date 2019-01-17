246ca11c311af9a02409d1cc167ce7b23dc4b6b7
Mon Maldonado <monterdi@gmail.com>
2019-01-17 22:51:28 +0100

    Presentando tkinter

---
+++ practicing_apps/tkinterTermoJuevesII/ejemploTkinter.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from tkinter import ttk
+
+class mainApp(Tk):
+    size = "1024x768"
+    
+    def __init__(self):
+        Tk.__init__(self)
+        
+        self.geometry(self.size)
+        self.title("Mi ventana")
+        self.configure(bg = "blue")
+
+    def start(self):
+        self.mainloop()
+
+if __name__ == '__main__':
+    app = mainApp()
+    
+    app.start()
+
+ttk.Button
+ttk.Radiobutton
+ttk.Entry
